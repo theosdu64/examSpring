683d846841aa1761b757c7d6d06197972318a063
theo <matchoc64600@gmail.com>
2026-04-07 11:10:45 +0200

    first commit

---
+++ examspring/src/test/java/com/example/examspring/ExamspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.examspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
