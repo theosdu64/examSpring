9fd25bef31793c8f608434838a8eb29732c7ccec
theo <matchoc64600@gmail.com>
2026-04-07 15:03:13 +0200

    ajout du service user avec les methode find,findall,save

---
+++ examspring/src/main/java/com/example/examspring/Service/UserService.java
@@ -0,0 +1,28 @@
+package com.example.examspring.Service;
+
+import com.example.examspring.Dao.UserRepository;
+import com.example.examspring.Entity.User;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class UserService {
+    private UserRepository userRepository;
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    public User save(User user) {
+        return userRepository.save(user);
+    }
+
+    public User findById(Long id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
+}
