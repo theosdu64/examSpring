d365d9d03563ea8ea40d0cca08ec5c516f7ff3a0
theo <matchoc64600@gmail.com>
2026-04-07 15:14:45 +0200

    add methode qui recupere un contact dun user a partir de sa category

--- examspring/src/main/java/com/example/examspring/Dao/ContactRepository.java
+++ examspring/src/main/java/com/example/examspring/Dao/ContactRepository.java
@@ -1,5 +1,6 @@
 package com.example.examspring.Dao;
 
+import com.example.examspring.Entity.Category;
 import com.example.examspring.Entity.Contact;
 import com.example.examspring.Entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,4 +9,5 @@ import java.util.List;
 
 public interface ContactRepository extends JpaRepository<Contact, Long> {
     List<Contact> findByUser(User user);
+    List<Contact> findByUserAndCategory(User user , Category category);
 }
