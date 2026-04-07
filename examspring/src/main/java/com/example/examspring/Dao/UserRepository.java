849a0bb348a574a7e841360f4e6c6c76a7f95060
theo <matchoc64600@gmail.com>
2026-04-07 15:07:49 +0200

    ajout de la methode findByEmail dans le repo de user

--- examspring/src/main/java/com/example/examspring/Dao/UserRepository.java
+++ examspring/src/main/java/com/example/examspring/Dao/UserRepository.java
@@ -4,4 +4,5 @@ import com.example.examspring.Entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    public User findByEmail(String email);
 }
