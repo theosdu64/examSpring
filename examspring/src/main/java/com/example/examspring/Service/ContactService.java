e7e01a04837dc1f04be4d2c91057ef6023e58883
theo <matchoc64600@gmail.com>
2026-04-07 15:21:49 +0200

    ajout des methode pour afficher les contact par category + tte les category par user + supprsion dune category

--- examspring/src/main/java/com/example/examspring/Service/ContactService.java
+++ examspring/src/main/java/com/example/examspring/Service/ContactService.java
@@ -1,7 +1,10 @@
 package com.example.examspring.Service;
 
 import com.example.examspring.Dao.ContactRepository;
+import com.example.examspring.Dao.UserRepository;
+import com.example.examspring.Entity.Category;
 import com.example.examspring.Entity.Contact;
+import com.example.examspring.Entity.User;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -22,5 +25,17 @@ public class ContactService {
 
     public Contact save(Contact contact) {
         return contactRepository.save(contact);
+    }
+
+    public List<Contact> findByCategory(User user, Category category) {
+        return contactRepository.findByUserAndCategory(user, category);
+    }
+
+    public List<Contact> findByUser(User user) {
+        return contactRepository.findByUser(user);
+    }
+
+    public void deleteById(Long id) {
+        contactRepository.deleteById(id);
     }
 }
