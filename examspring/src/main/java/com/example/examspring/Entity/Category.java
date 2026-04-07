e2286ac4db53b554381df2a579bf0b88019f4583
theo <matchoc64600@gmail.com>
2026-04-07 13:36:46 +0200

    creation de lentité category pour chaque contact

---
+++ examspring/src/main/java/com/example/examspring/Entity/Category.java
@@ -0,0 +1,25 @@
+package com.example.examspring.Entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @OneToMany(mappedBy = "category")
+    private List<Contact> contacts;
+}
