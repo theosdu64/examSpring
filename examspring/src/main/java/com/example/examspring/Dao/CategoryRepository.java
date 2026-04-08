8ac7704f18be67951f802beb4acf4bd32b49b496
theo <matchoc64600@gmail.com>
2026-04-08 10:40:26 +0200

    fix id type

--- examspring/src/main/java/com/example/examspring/Dao/CategoryRepository.java
+++ examspring/src/main/java/com/example/examspring/Dao/CategoryRepository.java
@@ -3,5 +3,5 @@ package com.example.examspring.Dao;
 import com.example.examspring.Entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CategoryRepository extends JpaRepository<Category, Integer> {
+public interface CategoryRepository extends JpaRepository<Category, Long> {
 }
