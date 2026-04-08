6f61f2abb7c813f849a96806ff5cd1259d5e9f69
theo <matchoc64600@gmail.com>
2026-04-08 09:48:16 +0200

    ajout de la methode daffichage et creation du controller contact

--- examspring/src/main/java/com/example/examspring/Controller/ContactController.java
+++ examspring/src/main/java/com/example/examspring/Controller/ContactController.java
@@ -1,4 +1,22 @@
 package com.example.examspring.Controller;
 
+import org.springframework.ui.Model;
+import com.example.examspring.Entity.User;
+import jakarta.servlet.http.HttpSession;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@AllArgsConstructor
 public class ContactController {
+
+    @GetMapping("/contacts")
+    public String list(HttpSession session, Model model) {
+        User user = (User) session.getAttribute("user");
+        if (user == null) {
+            return "redirect:/login";
+        }
+        return "contacts";
+    }
 }
